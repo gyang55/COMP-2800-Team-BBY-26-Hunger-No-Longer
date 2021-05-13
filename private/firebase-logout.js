589ea85bfeb807bb12ab32f44a72b75d7231d463
Johnny Hui <johnny_hui@live.com>
2021-05-12 22:21:25 -0700

    new stuff.

---
+++ private/firebase-logout.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    firebase.auth().signOut().then (function () {
+        console.log("user had signed out")
+        window.location.href = "index.html";
+    })
+    .catch(function (error) {
+        // An error happened
+    });
+})
