46affdfc2f8e817b62b80d368d5c4d26eadbcac7
Alex Wong <wongalexjx@gmail.com>
2021-05-16 23:31:30 -0700

    added 5 sec refresh for volunteer confirmation

---
+++ volunteer/js/redirectFiveSecs.js
@@ -0,0 +1,7 @@
+
+//Using setTimeout to execute a function after 5 seconds.
+setTimeout(function () {
+   //Redirect with JavaScript
+   window.location.href= 'https://hunger-no-longer.web.app/individual.html';
+   //window.location.replace ='https://hunger-no-longer.web.app/individual.html';
+}, 5000);
