2b0466bdf28c7e54c424c080e58c12562eb29aee
Johnny Hui <johnny_hui@live.com>
2021-05-20 14:06:22 -0700

    Fixed the redirectories again.

--- private/firebase-logout.js
+++ private/firebase-logout.js
@@ -21,7 +21,7 @@ $(function() {
 function logOut() {
     firebase.auth().signOut()
         .then(function() {
-            window.location.href = "/getStarted/index.html";
+            window.location.href = "/index.html";
         })
         .catch(function(error) {
             // An error happened
