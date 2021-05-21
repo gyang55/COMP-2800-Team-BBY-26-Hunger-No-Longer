21e59e25c3f58eee44c0023275442e4d2dadd5ec
Alex Wong <wongalexjx@gmail.com>
2021-05-21 01:34:15 -0700

    added available days to volunteer.js / volunteer-reg.html

--- volunteer/js/redirectFiveSecs.js
+++ volunteer/js/redirectFiveSecs.js
@@ -2,6 +2,6 @@
 //Using setTimeout to execute a function after 5 seconds.
 setTimeout(function () {
    //Redirect with JavaScript
-   window.location.href= 'https://hunger-no-longer.web.app/individual.html';
+   window.location.href= 'https://hunger-no-longer.web.app/Forum/Forum.html';
    //window.location.replace ='https://hunger-no-longer.web.app/individual.html';
 }, 5000);
